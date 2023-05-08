d05c3a885ac9e018efa44fbcc2cff49a143757b2
kenlove <cherestek@gmail.com>
2023-05-07 23:36:16 -0300

    first commit

---
+++ teste.01.py
@@ -0,0 +1,9 @@
+#Algoritmo
+
+n = int(input('Digite um numero: '))
+d = n * 2
+t = n * 3
+r = n **(1/2)
+print('o dobro de {} vale {},'.format(n, d))
+print('o triplo de {} vale {},'.format(n, t))
+print('a raiz quadrada de {} é igual a {:.2f},'.format(n,r))
---
+++ teste.02.py
@@ -0,0 +1,6 @@
+#Nota de um Aluno
+
+n1 = float(input('Primeira nota do aluno: '))
+n2 = float(input('Segunda nota do aluno: '))
+media = (n1 + n2 )/ 2
+print('A media entre {:.1f} e {:.1f} é igual a {:.5.51f}'.format(n1, n2, media))
